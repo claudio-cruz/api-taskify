10b6458d02a292b2b10b5ff8f09830c953702495
Claudio Cruz <claudiocruz1989@gmail.com>
2023-02-06 18:14:45 +0000

    Add permissions to profiles views

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -4,10 +4,15 @@ from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source='user.username')
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.user
 
     class Meta:
         model = Profile
         fields = [
             'id', 'user', 'created_at', 'updated_at', 'name',
-            'bio', 'profile_pic'
+            'bio', 'profile_pic', 'is_owner'
         ]

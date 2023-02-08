74ff82e0079bc96b9e834b74164469e0f5c58738
Claudio Cruz <claudiocruz1989@gmail.com>
2023-02-08 18:10:29 +0000

    Add TaskDetail class to tasks views

--- tasks/views.py
+++ tasks/views.py
@@ -1,13 +1,15 @@
+from django.http import Http404
 from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Task
 from .serializers import TaskSerializer
+from api_taskify.permissions import IsOwner
 
 
 class TaskList(APIView):
     """
-    List all tasks.
+    List all tasks, create new tasks.
     """
     serializer_class = TaskSerializer
     permission_classes = [
@@ -32,4 +34,46 @@ class TaskList(APIView):
             )
         return Response(
             serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+class TaskDetail(APIView):
+    """
+    Retrieve, update or delete a task instance.
+    """
+    permission_classes = [IsOwner]
+    serializer_class = TaskSerializer
+
+    def get_object(self, pk):
+        try:
+            task = Task.objects.get(pk=pk)
+            self.check_object_permissions(self.request, task)
+            return task
+        except Task.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        task = self.get_object(pk)
+        serializer = TaskSerializer(
+            task, context={'request': request}
+        )
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        task = self.get_object(pk)
+        serializer = TaskSerializer(
+            task, data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
+
+    def delete(self, request, pk):
+        task = self.get_object(pk)
+        task.delete()
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
         )

51734200f7454aade1c0adc9a8ce7217af6cc8d5
Claudio Cruz <claudiocruz1989@gmail.com>
2023-02-12 18:29:04 +0000

    Creat notes app and add note model

--- api_taskify/settings.py
+++ api_taskify/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
     'tasks',
     'events',
     'habits',
+    'notes',
 ]
 
 MIDDLEWARE = [
---
+++ notes/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+
+PRIORITY_CHOICES = [
+    ('low', 'Low'),
+    ('medium', 'Medium'),
+    ('high', 'High'),
+]
+
+CATEGORY_CHOICES = [
+    ('other', 'Other'),
+    ('study', 'Study'),
+    ('finance', 'Finance'),
+    ('work', 'Work'),
+    ('sport', 'Sport'),
+    ('social', 'Social'),
+    ('home', 'Home'),
+]
+
+
+class Note(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES)
+    category = models.CharField(max_length=10, choices=CATEGORY_CHOICES)
+
+    class Meta:
+        ordering = ['-created_at', '-updated_at']
+
+    def __str__(self):
+        return f'{self.id} {self.note}'
+
+
+def create_note(sender, instance, created, **kwargs):
+    if created:
+        Note.objects.create(user=instance)
+
+
+post_save.connect(create_note, sender=User)

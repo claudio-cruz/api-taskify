27f587fdde59e2172cc5854894455e9f612f53a4
Claudio Cruz <claudiocruz1989@gmail.com>
2023-03-03 18:36:38 +0000

    Set DEBUG to True

--- api_taskify/settings.py
+++ api_taskify/settings.py
@@ -61,7 +61,7 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEV' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
